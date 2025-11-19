9a2b670c8ac1a3b7f408c6776c36b65a8d7c3675
Syr873 <mtroi873@gmail.com>
2025-11-18 23:49:27 -0300

    Inicia as funçoes da aula 02

---
+++ aula02/desafios.py
@@ -0,0 +1,10 @@
+#desafio_01
+vendas = float(input('Informe o valor vendido atual: '))
+meta = 15000 - vendas
+print(f'Sua quantia acumulada em vendas atualmente é {vendas} \nFaltam {meta} em vendas até a meta ser atingida!')
+
+#desafio_02
+# preço = float(input('informe o valor antigo do produto: '))
+# promoçao = preço*(13/100)
+# valor_atual = preço - promoçao
+# print(f'O preço informado: {preço} \nDesconto: {promoçao:.2f} \nPreço final: {valor_atual}')
---
+++ aula02/exercicios.py
@@ -0,0 +1,36 @@
+#exercicio_01
+# numero = int(input('digite um numero: '))
+# antecessor = numero - 1
+# sucessor = numero + 1
+# print(f'Antecessor: {antecessor} \nSucessor: {sucessor}')
+
+#exercicio_02
+# numero = float(input('Digite um numero: '))
+# dobro = 2*numero
+# triplo = 3*numero
+# quadrado = numero**2
+# print(f'Dobro: {dobro} \nTriplo: {triplo} \nQuadrado: {quadrado}')
+
+#exercicio_03
+# medida = float(input('Digite a medida(metros): '))
+# centimetro = medida * 100
+# milimetro = medida * 1000
+# print(f'Centimetros: {centimetro} \nMilimetro: {milimetro}')
+
+#exercicio_04
+# nome = input('Digite seu nome: ')
+# nota_01 = float(input('Digite a primeira nota: '))
+# nota_02 = float(input('Digite a segunda nota: '))
+# media = (nota_01 + nota_02)/2
+# print(f'Aluno(a) {nome} \nMedia: {media}')
+
+#exxercicio_05
+# num_01 = float(input('Digite o primeiro numero: '))
+# num_02 = float(input('Digite o segundo numero: '))
+# soma = num_01 + num_02
+# sub = num_01 - num_02
+# divide = num_01/num_02
+# multi = num_01 * num_02
+# modulo = num_01%num_02
+# print(f'Seus numeros: {num_01} e {num_02} \nsoma: {soma} \nSubtraçao: {sub} \nDivisao: {divide} \nMultiplicaçao: {multi} \nModulo: {modulo}')
+
