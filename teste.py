33728048205c8ae1e57d23e58a3424791cb4a95d
Syr873 <mtroi873@gmail.com>
2025-11-21 16:14:41 -0300

    Adiciona os arquivos da aula01

--- teste.py
+++ teste.py
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 # Testando
 
 print('Arquivo teste')
 print('Printei')
 
-=======
-# Testando
 
-print('Arquivo teste')
-print('Printei')
-
->>>>>>> 37c8dcddcb66f5e6983c987292062a33867305e5
+"Olá"
